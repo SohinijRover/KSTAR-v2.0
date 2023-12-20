745d83ad39cf5d4bd795b9041d4b018f3fca2221
gauravtripathii <gauravtripathii>
2023-12-20 11:40:15 +0530

    responsive final

--- src/components/Home/Sponcers.js
+++ src/components/Home/Sponcers.js
@@ -8,22 +8,27 @@ function Sponcers() {
         <img
           src="/assets/Sponcers/sponcer1.svg"
           alt="sponcer1"
+          className="2xl:w-3/12"
         />
         <img
           src="/assets/Sponcers/sponcer2.svg"
           alt="sponcer2"
+          className="2xl:w-3/12"
         />
         <img
           src="/assets/Sponcers/sponcer3.svg"
           alt="sponcer3"
+          className="2xl:w-3/12"
         />
         <img
           src="/assets/Sponcers/sponcer4.svg"
           alt="sponcer4"
+          className="2xl:w-3/12"
         />
         <img
           src="/assets/Sponcers/sponcer5.svg"
           alt="sponcer5"
+          className="2xl:w-3/12"
         />
       </div>
     </div>
