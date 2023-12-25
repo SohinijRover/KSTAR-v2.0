00fb1ce4208fdc080a1f4d10e2cd9f5255d3047e
pratik-win-PC <pratiktravelogue@gmail.com>
2023-12-25 13:35:29 +0530

    all pages made responsive and links verified

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -3,50 +3,55 @@ import { Link } from "react-router-dom";
 
 function Music() {
   return (
-    <div className="bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,#AC1D39_0%,#3A130E_100%)] w-full h-screen flex flex-col px-20 pt-5">
-  <div className="flex justify-between">
-    <img
-      src="/assets/Challenges/main-logo.svg"
-      alt="challenge logo"
-      className="w-96"
-    />
-    <div className="flex items-start pt-5 gap-10">
-      <Link to="/" className="flex items-center gap-3 text-gold font-bold text-2xl">
-        <img src="/assets/Challenges/home.svg" alt="home icon" />
-        Home
-      </Link>
-      <a className="flex items-center gap-3 text-gold font-bold text-2xl" href="mailto:studentsupport@kiit.ac.in?subject=Enquiry - KSTAR">
-        <img src="/assets/Challenges/chat.svg" alt="chat icon" />
-        Contact Us
-      </a>
-    </div>
-  </div>
+    <div className="bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,#AC1D39_0%,#3A130E_100%)] w-full min-h-screen flex flex-col px-4 sm:px-6 lg:px-8 pt-5">
+      <div className="flex flex-col sm:flex-row items-center justify-between lg:p-5">
+        <div className="mb-4 sm:mb-0 sm:mr-4">
+          <img
+            src="/assets/Challenges/main-logo.svg"
+            alt="challenge logo"
+            className="w-48 sm:w-64 md:w-80 lg:w-96"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:p-5 gap-5">
+          <Link to="/" className="flex items-center gap-3 text-gold font-bold text-xl sm:text-2xl">
+            <img src="/assets/Challenges/home.svg" alt="home icon" />
+            <div>Home</div>
+          </Link>
+          <a className="flex items-center gap-3 text-gold font-bold text-xl sm:text-2xl" href="mailto:studentsupport@kiit.ac.in?subject=Enquiry - KSTAR">
+            <img src="/assets/Challenges/chat.svg" alt="chat icon" />
+            <div>Contact Us</div>
+          </a>
+        </div>
+      </div>
 
-  <div className="h-full flex items-center justify-between">
-    <div className="w-6/12 h-full flex flex-col justify-center gap-10">
-      <div className="text-gold font-bold text-6xl">MUSIC</div>
-      <div className="text-white font-bold text-2xl">
-      “To play a wrong note is insignificant .To play without passion is inexcusable”-Beethovan If you are a musician and feel like the mundane routine has toned down your passion and love for music, then hold your fire no more.The stage is all set for you to mesmerize everyone with your euphonious voice. To be the best instrumentalist or vocalist among your peers. Let this wonderful event encourage you to sustain your passion , love and proclivity for music.
-            </div>
-      <div className="flex gap-10">
-      <a href="https://forms.gle/vXbdUriYkmxKgj1w5"
-              className="bg-gold text-red-main font-bold text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between p-5">
+        <div className="w-full lg:w-6/12 h-full flex flex-col justify-center gap-5 text-center sm:text-left">
+          <div className="text-gold font-bold text-4xl md:text-6xl lg:p-5">
+            MUSIC
+          </div>
+          <div className="text-white font-bold text-lg md:text-2xl lg:text-xl lg:p-5">
+
+            To play a wrong note is insignificant. To play without passion is inexcusable. - Beethoven If you are a musician and feel like the mundane routine has toned down your passion and love for music, then hold your fire no more. The stage is all set for you to mesmerize everyone with your euphonious voice. To be the best instrumentalist or vocalist among your peers. Let this wonderful event encourage you to sustain your passion, love, and proclivity for music.
+          </div>
+          <div className="flex flex-col sm:flex-row gap-5 lg:p-5">
+            <a href="https://forms.gle/vXbdUriYkmxKgj1w5"
+              className="bg-gold text-red-main font-bold text-xl sm:text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
               target="_blank" rel="noreferrer">
               Submit Here
             </a>
             <a href="https://drive.google.com/file/d/1_q0zPk4927WVNQ2lDClQtKntAvZuAHDw/view?usp=drive_link"
-              className="bg-gold text-red-main font-bold text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
+              className="bg-gold text-red-main font-bold text-xl sm:text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
               target="_blank" rel="noreferrer">
               Theme & Guidelines
             </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-40 sm:w-30 h-50 lg:pr-10">
+          <img src="/assets/Challenges/music.svg" alt="music" className="w-full" />
+        </div>
       </div>
     </div>
-    <div className="w-1/5 h-1/7 pr-16">
-      <img src="/assets/Challenges/music.svg" alt="music" className="w-full" />
-    </div>
-  </div>
-</div>
-);
+  );
 }
 
 export default Music;
--- src/pages/ShortFilm.js
+++ src/pages/ShortFilm.js
@@ -3,50 +3,59 @@ import { Link } from "react-router-dom";
 
 function ShortFilm() {
   return (
-    <div className="bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,#AC1D39_0%,#3A130E_100%)] w-full h-screen flex flex-col px-20 pt-5">
-  <div className="flex justify-between">
-    <img
-      src="/assets/Challenges/main-logo.svg"
-      alt="challenge logo"
-      className="w-96"
-    />
-    <div className="flex items-start pt-5 gap-10">
-      <Link to="/" className="flex items-center gap-3 text-gold font-bold text-2xl">
-        <img src="/assets/Challenges/home.svg" alt="home icon" />
-        Home
-      </Link>
-      <a className="flex items-center gap-3 text-gold font-bold text-2xl" href="mailto:studentsupport@kiit.ac.in?subject=Enquiry - KSTAR">
-        <img src="/assets/Challenges/chat.svg" alt="chat icon" />
-        Contact Us
-      </a>
-    </div>
-  </div>
+    <div className="bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,#AC1D39_0%,#3A130E_100%)] w-full min-h-screen flex flex-col px-4 sm:px-6 lg:px-8 pt-5">
+      <div className="flex flex-col sm:flex-row items-center justify-between lg:p-5">
+        <div className="mb-4 sm:mb-0 sm:mr-4">
+          <img
+            src="/assets/Challenges/main-logo.svg"
+            alt="challenge logo"
+            className="w-48 sm:w-64 md:w-80 lg:w-96"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:p-5 gap-5">
+          <Link to="/" className="flex items-center gap-3 text-gold font-bold text-xl sm:text-2xl">
+            <img src="/assets/Challenges/home.svg" alt="home icon" />
+            <div>Home</div>
+          </Link>
+          <a className="flex items-center gap-3 text-gold font-bold text-xl sm:text-2xl" href="mailto:studentsupport@kiit.ac.in?subject=Enquiry - KSTAR">
+            <img src="/assets/Challenges/chat.svg" alt="chat icon" />
+            <div>Contact Us</div>
+          </a>
+        </div>
+      </div>
 
-  <div className="h-full flex justify-between">
-    <div className="w-6/12 h-full flex flex-col justify-center gap-10">
-      <div className="text-gold font-bold text-6xl">SHORT FILM</div>
-      <div className="text-white font-bold text-2xl">
-      Do you know the ideology behind the concept of short films “ALL TOO WELL(taylor’s short film)”?Is“THE HUNT” of a good short film enough to bond “TWO DISTANT STRANGERS”.If you think so too and see yourself as the next Anurag Kashyap or Tarantino , then you my friend are at the right place.Kstar brings to the table a unique event that intends to discover and honour the very best and rising filmmakers among us.
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between p-5">
+        <div className="w-full lg:w-6/12 h-full flex flex-col justify-center gap-5 text-center sm:text-left">
+          <div className="text-gold font-bold text-4xl md:text-6xl lg:p-5">
+           SHORTFILM
+          </div>
+          <div className="text-white font-bold text-lg md:text-2xl lg:text-xl lg:p-5">
+
+          Do you know the ideology behind the concept of short films “ALL TOO WELL(taylor’s short film)”?Is“THE HUNT” of a good short film enough to bond “TWO DISTANT STRANGERS”.If you think so too and see yourself as the next Anurag Kashyap or Tarantino , then you my friend are at the right place.Kstar brings to the table a unique event that intends to discover and honour the very best and rising filmmakers among us.
             </div>
-      <div className="flex gap-10">
-      <a href="https://forms.gle/kERg1koE6sVQfxGRA"
-              className="bg-gold text-red-main font-bold text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
+          <div className="flex flex-col sm:flex-row gap-5 lg:p-5">
+            <a href="https://forms.gle/vXbdUriYkmxKgj1w5"
+              className="bg-gold text-red-main font-bold text-xl sm:text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
               target="_blank" rel="noreferrer">
               Submit Here
             </a>
             <a href="https://drive.google.com/file/d/1MvlwvyKlhF6hvZnydHULVABEUk4dzV9v/view?usp=drive_link"
-              className="bg-gold text-red-main font-bold text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
+              className="bg-gold text-red-main font-bold text-xl sm:text-2xl px-7 py-4 rounded-lg cursor-pointer border-2 border-gold hover:bg-inherit hover:text-gold"
               target="_blank" rel="noreferrer">
               Theme & Guidelines
             </a>
+          </div>
+        </div>
+        <div className="w-3/12 h-3/4 sm:w-4/12">
+          <img
+            src="/assets/Challenges/film.svg"
+            alt="film"
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
-    <div className="w-3/12 h-1/2 pt-24">
-      <img src="/assets/Challenges/film.svg" alt="film" className="w-full" />
-    </div>
-  </div>
-</div>
-);
+  );
 }
 
 export default ShortFilm;
